482c6908bc07958ee8dcb1fbe79a88f0c8d5496a
Vitoria Kathlyn Dias Ferreira <vkferreira@caloi.com>
2025-02-18 15:27:48 -0300

    soma e média

---
+++ soma e media/soma e media/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class somaEmedia 
+{
+    static void Main()
+    {
+        float nota = 0;
+
+        Console.WriteLine("quantas notas serão inseridas? (acima de 3 e abaico de 10)? ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        for(int i =1; i<=tamanho; i++)
+        {
+            Console.WriteLine($"digite a nota {i}");
+            nota += float.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"o resultado da média é : { nota / tamanho}");
+
+    }
+
+
+}
+
