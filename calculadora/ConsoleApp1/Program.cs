94dfb5ddc943bf31b17a339240356e85664754b6
Vitoria Kathlyn Dias Ferreira <vkferreira@caloi.com>
2025-02-18 14:50:11 -0300

    calculadora ex 1

---
+++ calculadora/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Programa
+{
+    static void Main()
+    {
+        Console.WriteLine("Calculadora");
+        Console.Write("qual operção você quer fazer? (1= + ,2= - ,3= x ,4= / ) ");
+        int operação = int.Parse(Console.ReadLine());
+
+        Console.Write("digite o primeiro numero: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("digite o segundo numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+
+
+        switch (operação)
+        {
+            case 1:
+                Console.WriteLine($"{numero} + {numero2} = {numero + numero2}");
+                break;
+            case 2:
+                Console.WriteLine($"{numero} - {numero2} = {numero - numero2}");
+                break;
+            case 3:
+                Console.WriteLine($"{numero} x {numero2} = {numero * numero2}");
+                break;
+            case 4:
+                Console.WriteLine($"{numero} / {numero2} = {numero / numero2}");
+                break;
+          
+            default:
+                Console.WriteLine("operação invalida");
+                break;
+        }
+
+
+      
+    }
+}
