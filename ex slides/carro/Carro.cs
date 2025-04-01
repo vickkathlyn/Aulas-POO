620ad1874f06e728f9ad89fd06d781b343c03393
vick kathlyn <Vickkathlyn@outlook.com>
2025-04-01 11:33:42 -0300

    cores carros

---
+++ ex slides/carro/Carro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Carro
+{
+    // Contadores estáticos para as cores
+    private static int vermelho = 0;
+    private static int azul = 0;
+    private static int amarelo = 0;
+
+    private static Random random = new Random();
+
+    public string Cor { get; set; }
+
+    public Carro()
+    {
+        List<string> cores = new List<string> { "vermelho", "azul", "amarelo" };
+        Cor = cores[random.Next(cores.Count)];
+
+        // Incrementa o contador da cor correspondente
+        if (Cor == "vermelho")
+        {
+            vermelho++;
+        }
+        else if (Cor == "azul")
+        {
+            azul++;
+        }
+        else
+        {
+            amarelo++;
+        }
+    }
+
+    // Método estático para exibir a quantidade de carros de cada cor
+    public static void ExibirCores()
+    {
+        Console.WriteLine($"Existem {vermelho} Carros da cor vermelha");
+        Console.WriteLine($"Existem {azul} Carros da cor azul");
+        Console.WriteLine($"Existem {amarelo} Carros da cor amarela");
+    }
+}
---
+++ ex slides/carro/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<Carro> carros = new List<Carro>();
+
+        // Criamos 1000 carros com cores aleatórias
+        for (int i = 0; i < 1000; i++)
+        {
+            carros.Add(new Carro());
+        }
+
+        // Agora, chamamos ExibirCores para exibir a quantidade de carros por cor
+        Carro.ExibirCores(); // Chamamos o método estático ExibirCores
+    }
+} 
